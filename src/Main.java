e48fbd37b9ade6529ee80e7bdf6705a666f500cc
Kevin R. Stravers <kevinrs@stud.ntnu.no>
2014-01-16 21:59:20 +0100

    Added init func

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 
 public class Main
 {
+	public void init()
+	{
+		System.out.println("Initialization fun");
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("heelosaouce!!!");
+		
 	}
 }
