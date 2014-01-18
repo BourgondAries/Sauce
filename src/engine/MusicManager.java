18f23290f876e9f4bcdb9e9265f13358f29a0f19
Kevin R. Stravers <kevinrs@stud.ntnu.no>
2014-01-18 20:03:13 +0100

    hange

---
+++ src/engine/MusicManager.java
@@ -0,0 +1,41 @@
+package engine;
+
+import org.jsfml.audio.*;
+
+
+public class MusicManager
+{
+	// Constructor
+	public MusicManager()
+	{
+		AudioPlayer.player.start(m_stream[0]);
+	}
+	
+	public void fade(String file)
+	{
+		
+	}
+	
+	public void play()
+	{
+		
+	}
+	
+	public void pause()
+	{
+		
+	}
+	
+	public void stop()
+	{
+		
+	}
+	
+	public void setFadeSpeed(int fadespeed_ms = 50)
+	{
+		
+	}
+	
+
+	
+}
