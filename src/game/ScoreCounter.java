0faa5b96ed16da2a8c61be3f664f865d7e66be5b
BourgondAries <macocio@gmail.com>
2014-04-02 15:08:50 +0200

    Nice transition in sky, moved sky.tga

--- src/game/ScoreCounter.java
+++ src/game/ScoreCounter.java
@@ -1,7 +1,12 @@
 package game;
 
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import org.jsfml.graphics.*;
+
+import engine.PathedFonts;
 
 
 /**
@@ -15,17 +20,23 @@ public class ScoreCounter implements Drawable
 {
 	
 	private Font m_font;
-	private Text m_text;
+	private Text m_text = new Text();
 	
 	
 	public ScoreCounter()
 	{
-		
+		try {
+			m_font = PathedFonts.getFont(Paths.get("../../res/pixelmix.ttf"));
+		} catch (IOException exc_obj) {
+			exc_obj.printStackTrace();
+		}
+		m_text.setFont(m_font);
+		m_text.setString("Derp");
 	}
 	
 	
 	public void draw(RenderTarget target, RenderStates states)
 	{
-		
+		target.draw(m_text);
 	}
 }
